e09ba6d8bf55eaf73d42b0a627a4e93bdef3137a
Nikia Gubenkov <n.gubenkov@gmail.com>
2019-07-14 19:53:45 +0300

    index allows to send list of coordinates and they will be located correctly

--- MapNews/views.py
+++ MapNews/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main.html', {'f':{'lat': 51.50072919999999, 'lng': -0.1246254}})
+    return render(request, 'main.html', {'location': [{'lat': -13.1631, 'lng': -72.5450}, {'lat': 51.50072919999999, 'lng': -0.1246254},
+                                                      {'lat': 53.5587, 'lng': 108.1650}, {'lat': 52.7229, 'lng': 23.6556}  ]} )
 
 def get_values(request):
     return True
